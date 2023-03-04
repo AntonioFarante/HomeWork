b5286d8af3a2de6ed63bd541d44a4ffb389ae109
Antonio <oz0969991415@gmail.com>
2023-03-04 14:44:20 +0200

    to check

--- src/main/java/module10/Task2.java
+++ src/main/java/module10/Task2.java
@@ -9,25 +9,26 @@ import java.util.*;
 
 public class Task2 {
     public static void main(String[] args) throws IOException {
-        File file = new File("newFile.txt");
-        InputStream fis = new FileInputStream(file);
-        Scanner scanner = new Scanner(fis);
+            File file = new File("newFile.txt");
+            InputStream fis = new FileInputStream(file);
+            Scanner scanner = new Scanner(fis);
 
-        List<User> userList = new ArrayList<>();
+            List<User> userList = new ArrayList<>();
 
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            String[] strings = line.split(" ");
-            userList.add(new User(strings[0], Integer.parseInt(strings[1])));
-        }
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] strings = line.split(" ");
+                userList.add(new User(strings[0], Integer.parseInt(strings[1])));
+            }
+        System.out.println("userList.get(0).getName() = " + userList.get(0).getName());
 
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String s = gson.toJson(userList);
-        System.out.println(s);
+            String s = gson.toJson(userList);
+            System.out.println(s);
 
-        FileWriter fileWriter= new FileWriter("user.json");
-        gson.toJson(userList, fileWriter);
-        fileWriter.close();
-    }
+            FileWriter fileWriter= new FileWriter("user.json");
+            gson.toJson(userList, fileWriter);
+            fileWriter.close();
+        }
 }
